1a270588a1c4ffa39b8d30a31a87e29585bf7496
Kevin Fallas <Kevin.fallas.b@gmail.com>
2020-05-09 11:17:45 -0600

    Cambia nombre controller principal

---
+++ application/controllers/Sitio.php
@@ -0,0 +1,16 @@
+<?php
+
+    Class Sitio extends CI_Controller{
+
+        public function __construct() {
+            parent::__construct();
+    
+        }
+
+        public function index() {
+            $this->load->view('sitio/Inicio');
+            
+        }
+    }
+
+?>
